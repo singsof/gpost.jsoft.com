2959eabad7458b4d04b9b2daf164bd89a5512dcd
Your Name <you@example.com>
2023-02-10 21:11:42 +0700

    de

--- routes/auth.php
+++ routes/auth.php
@@ -1,23 +1,23 @@
 <?php
 
-use App\Http\Controllers\Auth\CustomerLoginControllers;
+//use App\Http\Controllers\Auth\CustomerLoginControllers;
 use Illuminate\Support\Facades\Route;
 
 
 
 
 
-Route:: //middleware('guest')
-    prefix('auth')
-    ->name('auth.')
-    ->group(function () {
-        Route::prefix('customer')
-            ->name('customer.')
-            ->group(function () {
-                Route::get('/login', [CustomerLoginControllers::class, 'viewLogin'])->name('viewLogin');
-                Route::post('/login', [CustomerLoginControllers::class, 'customLogin'])->name('customLogin');
-                Route::get('/register', [CustomerLoginControllers::class, 'viewRegistration'])->name('viewRegistration');
-                Route::post('/register', [CustomerLoginControllers::class, 'customRegistration'])->name('customRegistration');
-                // Route::get('/logout', 'Logout')->name('logout');
-            });
-    });
+//Route:: //middleware('guest')
+//    prefix('auth')
+//    ->name('auth.')
+//    ->group(function () {
+//        Route::prefix('customer')
+//            ->name('customer.')
+//            ->group(function () {
+////                Route::get('/login', [CustomerLoginControllers::class, 'viewLogin'])->name('viewLogin');
+////                Route::post('/login', [CustomerLoginControllers::class, 'customLogin'])->name('customLogin');
+////                Route::get('/register', [CustomerLoginControllers::class, 'viewRegistration'])->name('viewRegistration');
+////                Route::post('/register', [CustomerLoginControllers::class, 'customRegistration'])->name('customRegistration');
+//                // Route::get('/logout', 'Logout')->name('logout');
+//            });
+//    });
--- routes/customer.php
+++ routes/customer.php
@@ -1,22 +1,22 @@
 <?php
 
-use App\Http\Controllers\Auth\CustomerLoginControllers;
+//use App\Http\Controllers\Auth\CustomerLoginControllers;
 use Illuminate\Support\Facades\Route;
 
 
 
 
 
-Route::prefix('view')
-    ->name('view.')
-    ->group(function () {
-        Route::prefix('customer')
-            ->name('customer.')
-            ->group(function () {
-                Route::get('/dashboard', function(){
-                    return view('customer.dashboard');
-                })->name('dashboard');
-                Route::post('/login', [CustomerLoginControllers::class, 'customLogin'])->name('customLogin');
-                // Route::get('/logout', 'Logout')->name('logout');
-            });
-    });
+//Route::prefix('view')
+//    ->name('view.')
+//    ->group(function () {
+//        Route::prefix('customer')
+//            ->name('customer.')
+//            ->group(function () {
+//                Route::get('/dashboard', function(){
+//                    return view('customer.dashboard');
+//                })->name('dashboard');
+//                Route::post('/login', [CustomerLoginControllers::class, 'customLogin'])->name('customLogin');
+//                // Route::get('/logout', 'Logout')->name('logout');
+//            });
+//    });
